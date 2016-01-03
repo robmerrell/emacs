fdab7abf2de1433008f3803b0596936f9fb54966
Rob Merrell <rmerrell@partnerfusion.com>
2016-01-03 15:06:57 -0700

    Switched commands for line commenting for muscle memory

--- lisp/rob-evil.el
+++ lisp/rob-evil.el
@@ -35,8 +35,8 @@
   "gs" 'magit-status
 
   ;; comments
-  "ci" 'evilnc-comment-or-uncomment-lines
-  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "cl" 'evilnc-comment-or-uncomment-lines
+  "ci" 'evilnc-quick-comment-or-uncomment-to-the-line
   "cc" 'evilnc-copy-and-comment-lines
   "cp" 'evilnc-comment-or-uncomment-paragraphs
   "cr" 'comment-or-uncomment-region
