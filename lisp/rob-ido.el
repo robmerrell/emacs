2ec0dd2c964fd97cd1f7e38fa160a8d21ad0d60a
Rob Merrell <rmerrell@partnerfusion.com>
2016-01-15 12:56:29 -0700

    Use up and down keys in ido mode

--- lisp/rob-ido.el
+++ lisp/rob-ido.el
@@ -2,9 +2,10 @@
 
 (ido-mode 1)
 (ido-everywhere 1)
-(ido-vertical-mode 1)
-
 (flx-ido-mode 1)
+
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
 
 (setq ido-show-dot-for-dired t)
 
