e39abdfe42a3f6b2e8ad866ec636e26173b6fbee
Rob Merrell <rmerrell@partnerfusion.com>
2016-06-03 13:34:21 -0600

    markdown follow link at point shortcut

--- lisp/rob-markdown.el
+++ lisp/rob-markdown.el
@@ -4,6 +4,7 @@
 
 ;; keybinds
 (evil-leader/set-key-for-mode 'markdown-mode
+  "<SPC>f" 'markdown-follow-link-at-point
   "<SPC>p" 'markdown-preview
   "<SPC>l" 'markdown-live-preview-mode)
 (setq markdown-command "marked")
